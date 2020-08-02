5de894719cf4137aca6490917d7969aa4dfb6a85
Gabriel Baliza <gabrielbaliza1@gmail.com>
2020-08-02 19:17:46 -0300

    Página de recuperação de senha está bonita agora (mas não igual ao figma)

--- front-end/src/pages/Reset/styles.js
+++ front-end/src/pages/Reset/styles.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
+  display: block;
+  background: #E5E5E5;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  position: fixed;
 `;
 
 export const Background = styled.div`
@@ -9,29 +14,32 @@ export const Background = styled.div`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background: #E5E5E5;
 
   height: 100vh;
 
-  > img {
-    width: 100%;
-    max-width: 40vw;
-  }
 `;
 
 export const Content = styled.div`
-  display: flex;
-
-  flex-direction: column;
-  justify-content: center;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
   align-items: center;
-  width: 100%;
-  max-width: 700px;
-  height: 100vh;
-  background-color: #069e15;
+  width: 500px;
+  height: 500px;
+  padding: 45px;
 
-  img {
-    width: 30vh;
-    margin-bottom: 40px;
+  background: #F0F0F0;
+  border: 1px solid #CDCDCD;
+  box-sizing: border-box;
+  border-radius: 20px;
+
+  > img {
+    width: 60%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    
   }
 
   form {
@@ -40,27 +48,98 @@ export const Content = styled.div`
     justify-content: center;
     align-items: center;
     width: 400px;
+    
 
-    margin-bottom: 20px;
+    font-size: 14px;
+
+    margin-bottom: 50px;
 
     div + div {
       margin-top: 8px;
     }
+    
+    input{
+      
+      margin-left: auto;
+      margin-right: auto;
+      width: 350px;
+      height: 40px;
+      
+    }
 
     button {
-      margin: 16px 0px;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 25px;
+      width: 200px;
+      height: 40px;
+      background-color: #E91E63;
+
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 16px;
+      text-align: center;
+
+      color: #FFFFFF;
     }
   }
 `;
-export const Back = styled.div`
-  display: flex;
-  justify-content: center;y
-  align-items: center;
-  margin-bottom: 10px;
-  margin-right: 10px;
-  font-weight: bold;
 
-  svg {
-    margin-right: 8px;
-  }
+export const Borda = styled.div`
+
+  border: 1px solid #CDCDCD;
+
+`;
+
+export const Texto1 = styled.div`
+  justify-text: certerd;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 23px;
+  margin-top: 25px;
+  color: #494949;
+
+
+`;
+
+export const Texto2 = styled.div`
+  margin-top: 20px;
+  justify-text: certerd;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+
+  color: #000000;
+
+
+`;
+
+export const SubCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5px;
+`;
+
+export const Sub = styled.div`
+
+link {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+
+  color: #0364D7;
+  margin-left: 25px;
+  margin-right: 25px;
+}
 `;
