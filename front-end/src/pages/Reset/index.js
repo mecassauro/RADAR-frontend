a03268190e6331dd1ef11592a5e0acf7c67afa09
adornelas <andredornelas23@gmail.com>
2020-08-03 19:30:51 -0300

    Adição de tooltip na page de reset e refatorações

--- front-end/src/pages/Reset/index.js
+++ front-end/src/pages/Reset/index.js
@@ -3,17 +3,23 @@ import React from 'react';
 import { FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { Link } from 'react-router-dom';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import imgLogo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { useFirebase } from '../../hooks/firebase';
 
-import { Container, Content, Sub, Borda, Texto1, Texto2, SubCenter} from './styles';
+import { Container, Content, Sub, Borda, Texto1, SubCenter} from './styles';
 
 function Reset() {
 
   const {resetPassword} = useFirebase();
+
+  const [open, setOpen] = React.useState(true);
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
 
   const handleSubmit = async ({email}) => {
     console.log(email);
@@ -26,12 +32,16 @@ function Reset() {
       <Content>
         <img src={imgLogo} alt="Logo" />
         <Form onSubmit={handleSubmit}>
-          <Texto1> Digite seu e-mail:</Texto1>
+          <Texto1> Redefina sua senha:</Texto1>
           <Borda>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           </Borda>
-          <Button type="submit">Enviar link</Button>
-          <Texto2>Enviaremos um link de recuperação de senha para o seu e-mail.</Texto2>
+          <Tooltip
+            title="E-mail enviado. Cheque sua caixa de entrada"
+            open={open}
+          >
+            <Button type="submit" onClick={handleTooltipOpen} >Enviar link</Button>
+          </Tooltip >
         </Form>
       </Content>
       <SubCenter>
@@ -41,8 +51,6 @@ function Reset() {
         </Sub>
       </SubCenter>   
       
-      
-
     </Container>
   );
 }
