89754b82f962023a15536410af8ed9488befcded
phe0 <pheofo@gmail.com>
2020-11-19 17:03:03 -0300

    change duplicated code for the use of DataInformer component

--- src/pages/UBSdata/index.js
+++ src/pages/UBSdata/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 
-import { ImArrowUp2, ImArrowDown2 } from 'react-icons/im';
 import { BsFillSquareFill } from 'react-icons/bs';
 
 import Header from '../../components/Header';
@@ -19,6 +18,8 @@ import Magnifier from '../../assets/Magnifier.svg';
 import Female from '../../assets/Female.svg';
 import Male from '../../assets/Male.svg';
 
+import DataInformer from '../../components/DataInformer';
+
 import {
   Container,
   PanelContainer,
@@ -32,7 +33,6 @@ import {
   Deaths,
   Sexes,
   Pdf,
-  MiniContainer,
 } from './styles';
 
 function UBS() {
@@ -103,69 +103,41 @@ function UBS() {
           </div>
         </DataContainer>
         <Content>
-          <MiniContainer>
-            <div>
-              <strong>Total de casos</strong>
-              <img src={User} alt="User" />
-            </div>
+          <DataInformer
+            title="Total de casos"
+            image={User}
+            alt="User"
+            quantity="42 mil"
+            percentage="1.3%"
+            direction="up"
+          />
 
-            <h1>42 mil</h1>
+          <DataInformer
+            title="Óbitos"
+            image={Pulse}
+            alt="Pulse"
+            quantity="5 mil"
+            percentage="0.9%"
+            direction="down"
+          />
 
-            <span>
-              <small style={{ color: '#f80303' }}>
-                <ImArrowUp2 size={11} color="#f80303" />
-                1.3 %
-              </small>
-              Desde o último mes
-            </span>
-          </MiniContainer>
+          <DataInformer
+            title="Curados"
+            image={Cure}
+            alt="Cure"
+            quantity="35 mil"
+            percentage="5.7%"
+            direction="down"
+          />
 
-          <MiniContainer>
-            <div>
-              <strong>Óbitos</strong>
-              <img src={Pulse} alt="Pulse" />
-            </div>
-
-            <h1>5 mil</h1>
-
-            <span>
-              <small style={{ color: '#04D361' }}>
-                <ImArrowDown2 size={11} color="#04D361" />
-                0.9 %
-              </small>
-              Desde o último mes
-            </span>
-          </MiniContainer>
-
-          <MiniContainer>
-            <div>
-              <strong>Curados</strong>
-              <img src={Cure} alt="Cure" />
-            </div>
-            <h1>35 mil</h1>
-            <span>
-              <small style={{ color: '#04D361' }}>
-                <ImArrowDown2 size={11} color="#04D361" />
-                5.7 %
-              </small>
-              Desde o último mes
-            </span>
-          </MiniContainer>
-
-          <MiniContainer>
-            <div>
-              <strong>Observações</strong>
-              <img src={Magnifier} alt="Magnifier" />
-            </div>
-            <h1>951</h1>
-            <span>
-              <small style={{ color: '#f80303' }}>
-                <ImArrowUp2 size={11} color="#f80303" />
-                8.3 %
-              </small>
-              Desde o último mes
-            </span>
-          </MiniContainer>
+          <DataInformer
+            title="Observações"
+            image={Magnifier}
+            alt="Magnifier"
+            quantity="951"
+            percentage="8.3%"
+            direction="up"
+          />
         </Content>
 
         <Content2>
